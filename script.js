51374fc9bcb55e47f068c4a9aed9e23ac2db68ca
Avinash <avinash.ponneboina@gmail.com>
2025-08-26 21:53:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
         document.addEventListener('DOMContentLoaded', function() {
             const canvas = document.getElementById('drawingCanvas');
             const ctx = canvas.getContext('2d');
-            const colorOptions = document.querySelectorAll('.color-option');
-            const customColor = document.getElementById('customColor');
-            const brushSizeInput = document.getElementById('brushSize');
+            const colorOptions = document.querySelectorAll('.color-option')
+            const customColor = document.getElementById('customColor')
+            const brushSizeInput = document.getElementById('brushSize')
             const brushPreview = document.getElementById('brushPreview');
             const clearCanvasBtn = document.getElementById('clearCanvas');
             const saveDrawingBtn = document.getElementById('saveDrawing');
@@ -143,4 +143,5 @@
                 link.click();
             });
         });
+
 
